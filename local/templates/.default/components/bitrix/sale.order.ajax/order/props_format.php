7773574dfbab1fa6045549c88a6c518da82f57fd
Sergey <st@webgk.ru>
2016-04-13 15:22:24 +0300

    hidding rlab block edit

--- local/templates/.default/components/bitrix/sale.order.ajax/order/props_format.php
+++ local/templates/.default/components/bitrix/sale.order.ajax/order/props_format.php
@@ -1,9 +1,7 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
-    if (!function_exists("showFilePropertyField"))
-    {
-        function showFilePropertyField($name, $property_fields, $values, $max_file_size_show=50000)
-        {
+    if (!function_exists("showFilePropertyField")){
+        function showFilePropertyField($name, $property_fields, $values, $max_file_size_show=50000){
             $res = "";
 
             if (!is_array($values) || empty($values))
@@ -11,12 +9,9 @@
                     "n0" => 0,
                 );
 
-            if ($property_fields["MULTIPLE"] == "N")
-            {
+            if ($property_fields["MULTIPLE"] == "N") {
                 $res = "<label for=\"\"><input type=\"file\" size=\"".$max_file_size_show."\" value=\"".$property_fields["VALUE"]."\" name=\"".$name."[0]\" id=\"".$name."[0]\"></label>";
-            }
-            else
-            {
+            } else {
                 $res = '
                 <script type="text/javascript">
                 function addControl(item)
@@ -50,17 +45,13 @@
         }
     }
 
-    if (!function_exists("PrintPropsForm"))
-    {
-        function PrintPropsForm($arSource = array(), $locationTemplate = ".default")
-        {
-            if (!empty($arSource))
-            {
+    if (!function_exists("PrintPropsForm")){
+        function PrintPropsForm($arSource = array(), $locationTemplate = ".default") {
+            if (!empty($arSource)){
             ?>
 
             <?
-                foreach ($arSource as $arProperties)
-                {
+                foreach ($arSource as $arProperties) {
                     if ($arProperties["TYPE"] != "LOCATION") {
                         $class =  "infoPunct";
                     }
@@ -85,8 +76,7 @@
                         <?}?>
 
                     <?
-                        if ($arProperties["TYPE"] == "CHECKBOX")
-                        {
+                        if ($arProperties["TYPE"] == "CHECKBOX"){
                         ?>
 
                         <div class="bx_block r1x3 pt8" <?if($arProperties["ID"] == 62 || $arProperties["ID"] == 63){?> style="display:none;"<?}?>>
@@ -97,9 +87,7 @@
                                 <?endif?>
                         </div>
                         <?
-                        }
-                        elseif ($arProperties["TYPE"] == "TEXT")
-                        {
+                        } elseif ($arProperties["TYPE"] == "TEXT"){
                             if($arProperties["CODE"]!="certificate" && $arProperties["CODE"]!="CODE_COUPON") {
 
                             ?>
@@ -132,15 +120,12 @@
                                 <input type="text" maxlength="250" size="<?=$arProperties["SIZE1"]?>" value="<?=$_SESSION["CUSTOM_COUPON"]["COUPON_ID"].'-'.$_SESSION["CUSTOM_COUPON"]["DEFAULT_COUPON"]?>" name="<?=$arProperties["FIELD_NAME"]?>" id="<?=$arProperties["FIELD_NAME"]?>">
                             </div>
                             <?}
-                        }
-                        elseif ($arProperties["TYPE"] == "SELECT")
-                        {
+                        } elseif ($arProperties["TYPE"] == "SELECT"){
                         ?>
                         <div class="bx_block r3x1">
                             <select name="<?=$arProperties["FIELD_NAME"]?>" id="<?=$arProperties["FIELD_NAME"]?>" size="<?=$arProperties["SIZE1"]?>">
                                 <?
-                                if ($arProperties["ID"] == 29 && empty($arProperties["DEFAULT_VALUE"]))
-                                { //метро для физлиц
+                                if ($arProperties["ID"] == 29 && empty($arProperties["DEFAULT_VALUE"])){ //метро для физлиц
                                 ?>
                                     <option value="" selected="selected">Выберите станцию метро</option>
                                 <?
@@ -155,9 +140,7 @@
                                 <?endif?>
                         </div>
                         <?
-                        }
-                        elseif ($arProperties["TYPE"] == "MULTISELECT")
-                        {
+                        } elseif ($arProperties["TYPE"] == "MULTISELECT") {
                         ?>
                         <div class="bx_block r3x1">
                             <select multiple name="<?=$arProperties["FIELD_NAME"]?>" id="<?=$arProperties["FIELD_NAME"]?>" size="<?=$arProperties["SIZE1"]?>">
@@ -170,9 +153,7 @@
                                 <?endif?>
                         </div>
                         <?
-                        }
-                        elseif ($arProperties["TYPE"] == "TEXTAREA")
-                        {
+                        } elseif ($arProperties["TYPE"] == "TEXTAREA") {
                             $rows = ($arProperties["SIZE2"] > 10) ? 4 : $arProperties["SIZE2"];
                         ?>
                         <div class="bx_block r3x1">
@@ -183,33 +164,26 @@
                             <span class="warningMessage">Заполните поле <?=$arProperties["NAME"]?></span>
                         </div>
                         <?
-                        }
-                        elseif ($arProperties["TYPE"] == "LOCATION")
-                        {
+                        } elseif ($arProperties["TYPE"] == "LOCATION") {
                         ?>
                         <?
                             $value = 0;
-                            if (is_array($arProperties["VARIANTS"]) && count($arProperties["VARIANTS"]) > 0)
-                            {
-                                foreach ($arProperties["VARIANTS"] as $arVariant)
-                                {
-                                    if ($arVariant["SELECTED"] == "Y")
-                                    {
+                            if (is_array($arProperties["VARIANTS"]) && count($arProperties["VARIANTS"]) > 0){
+                                foreach ($arProperties["VARIANTS"] as $arVariant){
+                                    if ($arVariant["SELECTED"] == "Y"){
                                         $value = $arVariant["ID"];
                                         break;
                                     }
 
                                 }
-                                if ($value == "")
-                                {
+                                if ($value == ""){
                                     $value = $arProperties["VALUE"];
                                 }
                             }
 
                             // here we can get '' or 'popup'
                             // map them, if needed
-                            if(CSaleLocation::isLocationProMigrated())
-                            {
+                            if(CSaleLocation::isLocationProMigrated()){
                                 $locationTemplateP = $locationTemplate == 'popup' ? 'search' : 'steps';
                                 $locationTemplateP = $_REQUEST['PERMANENT_MODE_STEPS'] == 1 ? 'steps' : $locationTemplateP; // force to "steps"
                             }
@@ -261,14 +235,11 @@
                             <div class="bx_description"><?=$arProperties["DESCRIPTION"]?></div>
                             <?endif?>
                         <?
-                        }
-                        elseif ($arProperties["TYPE"] == "RADIO")
-                        {
+                        } elseif ($arProperties["TYPE"] == "RADIO") {
                         ?>
                         <div class="bx_block r3x1">
                             <?
-                                if (is_array($arProperties["VARIANTS"]))
-                                {
+                                if (is_array($arProperties["VARIANTS"])){
                                     foreach($arProperties["VARIANTS"] as $arVariants):
                                     ?>
                                     <input
@@ -279,7 +250,7 @@
 
                                     <label for="<?=$arProperties["FIELD_NAME"]?>_<?=$arVariants["VALUE"]?>"><?=$arVariants["NAME"]?></label></br>
                                     <?
-                                        endforeach;
+                                    endforeach;
                                 }
                             ?>
                             <?if (strlen(trim($arProperties["DESCRIPTION"])) > 0):?>
@@ -287,9 +258,7 @@
                                 <?endif?>
                         </div>
                         <?
-                        }
-                        elseif ($arProperties["TYPE"] == "FILE")
-                        {
+                        } elseif ($arProperties["TYPE"] == "FILE"){
                         ?>
                         <div class="bx_block r3x1">
                             <?=showFilePropertyField("ORDER_PROP_".$arProperties["ID"], $arProperties, $arProperties["VALUE"], $arProperties["SIZE1"])?>
@@ -298,9 +267,7 @@
                                 <?endif?>
                         </div>
                         <?
-                        }
-                        elseif ($arProperties["TYPE"] == "DATE")
-                        {
+                        } elseif ($arProperties["TYPE"] == "DATE"){
                         ?>
                         <div>
                             <?
