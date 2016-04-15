d2892d29f7f3dd7d7291b50824d5e3f5cabb79c2
Admin <git@webgk.ru>
2016-04-15 18:04:46 +0300

    update

--- test-section/index.php
+++ test-section/index.php
@@ -2,6 +2,18 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тестовый раздел");
 
-CSaleOrder::Update("65295", array("USER_DESCRIPTION" => "test"));
+$authorsList = CIBlockElement::GetList (array(), array("IBLOCK_ID" => 29), false, false, array("ID", "DETAIL_TEXT"));
+while ($authors = $authorsList -> Fetch()) {
+    CIBlockElement::SetPropertyValuesEx ($authors["ID"], 29, array ("AUTHOR_DETAIL_INFO" => $authors["DETAIL_TEXT"])); 
+    
+    $currAuthor = new CIBlockElement;
+    
+    $arLoadProductArray = Array(
+        "DETAIL_TEXT" => ""
+        );
+        
+    $res = $currAuthor -> Update ($authors["ID"], $arLoadProductArray);
+       
+}          
 
 ?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
