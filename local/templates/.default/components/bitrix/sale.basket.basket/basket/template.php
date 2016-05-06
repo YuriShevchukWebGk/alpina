95a9a7a2f82f343d7211e9ea3df4d2bac7bda6af
Ivan <mi@webgk.ru>
2016-05-06 16:34:39 +0300

    SUP-3108: Change gift block in basket

--- local/templates/.default/components/bitrix/sale.basket.basket/basket/template.php
+++ local/templates/.default/components/bitrix/sale.basket.basket/basket/template.php
@@ -46,49 +46,7 @@
         </script>
         <?
             $APPLICATION->AddHeadScript($templateFolder."/script.js");
-            
-            if($arParams['USE_GIFTS'] == 'Y')
-            {   
-                $APPLICATION->IncludeComponent(
-                    "bitrix:sale.gift.basket",
-                    "basket_gifts",
-                    array(
-                        "SHOW_PRICE_COUNT" => 1,
-                        "PRODUCT_SUBSCRIPTION" => 'N',
-                        'PRODUCT_ID_VARIABLE' => 'id',
-                        "PARTIAL_PRODUCT_PROPERTIES" => 'N',
-                        "USE_PRODUCT_QUANTITY" => 'N',
-                        "ACTION_VARIABLE" => "actionGift",
-                        "ADD_PROPERTIES_TO_BASKET" => "Y",
 
-                        "BASKET_URL" => $APPLICATION->GetCurPage(),
-                        "APPLIED_DISCOUNT_LIST" => $arResult["APPLIED_DISCOUNT_LIST"],
-                        "FULL_DISCOUNT_LIST" => $arResult["FULL_DISCOUNT_LIST"],
-
-                        "TEMPLATE_THEME" => $arParams["TEMPLATE_THEME"],
-                        "PRICE_VAT_INCLUDE" => $arParams["PRICE_VAT_SHOW_VALUE"],
-                        "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-
-                        'BLOCK_TITLE' => $arParams['GIFTS_BLOCK_TITLE'],
-                        'HIDE_BLOCK_TITLE' => $arParams['GIFTS_HIDE_BLOCK_TITLE'],
-                        'TEXT_LABEL_GIFT' => $arParams['GIFTS_TEXT_LABEL_GIFT'],
-                        'PRODUCT_QUANTITY_VARIABLE' => $arParams['GIFTS_PRODUCT_QUANTITY_VARIABLE'],
-                        'PRODUCT_PROPS_VARIABLE' => $arParams['GIFTS_PRODUCT_PROPS_VARIABLE'],
-                        'SHOW_OLD_PRICE' => $arParams['GIFTS_SHOW_OLD_PRICE'],
-                        'SHOW_DISCOUNT_PERCENT' => $arParams['GIFTS_SHOW_DISCOUNT_PERCENT'],
-                        'SHOW_NAME' => $arParams['GIFTS_SHOW_NAME'],
-                        'SHOW_IMAGE' => $arParams['GIFTS_SHOW_IMAGE'],
-                        'MESS_BTN_BUY' => $arParams['GIFTS_MESS_BTN_BUY'],
-                        'MESS_BTN_DETAIL' => $arParams['GIFTS_MESS_BTN_DETAIL'],
-                        'PAGE_ELEMENT_COUNT' => $arParams['GIFTS_PAGE_ELEMENT_COUNT'],
-                        'CONVERT_CURRENCY' => $arParams['GIFTS_CONVERT_CURRENCY'],
-                        'HIDE_NOT_AVAILABLE' => $arParams['GIFTS_HIDE_NOT_AVAILABLE'],
-
-                        "LINE_ELEMENT_COUNT" => $arParams['GIFTS_PAGE_ELEMENT_COUNT'],
-                    ),
-                    false
-                );
-            }
 
         ?>
 
@@ -159,7 +117,7 @@
                                 }
                                 arshow($WishFilter);*/
                                 $uID = $USER -> GetID();
-                                
+
                                 if ($_REQUEST["list"]) {
                                     $list_user = CUser::GetByID($_REQUEST["list"]) -> Fetch();
                                     $list_user_name = $list_user["NAME"]." ".$list_user["LAST_NAME"];
@@ -168,7 +126,7 @@
                                     $list_user = CUser::GetByID($uID) -> Fetch();
                                     $list_user_name = $list_user["NAME"]." ".$list_user["LAST_NAME"];   
                                 }
-                                
+
                                 global $WishFilter;
                                 $WishFilter["NAME"] = $list_user_name;
                                 $APPLICATION->IncludeComponent(
@@ -305,46 +263,46 @@
                 <input type="hidden" name="BasketOrder" value="BasketOrder" />
                 <!-- <input type="hidden" name="ajax_post" id="ajax_post" value="Y"> -->
             </form>
-			
-			<script type="text/javascript">
-				dataLayer.push({
-					'ecommerce': {
-						'checkout': {
-							'actionField': {'step':1},
-							'products': [
-								<?foreach($_SESSION['gtmEnchECommerceCheckout'] as $googleItem){?>
-									{
-										<?=$googleItem?>
-									},
-								<?}?>
-							]
-						}
-					}
-				});
 
-			</script>
-			
-			<script type="text/javascript" src="//static.criteo.net/js/ld/ld.js" async="true"></script>
-            
-			<script type="text/javascript">
-				window.criteo_q = window.criteo_q || [];
-				window.criteo_q.push(
-				{ event: "setAccount", account: 18519 },
+            <script type="text/javascript">
+                dataLayer.push({
+                    'ecommerce': {
+                        'checkout': {
+                            'actionField': {'step':1},
+                            'products': [
+                                <?foreach($_SESSION['gtmEnchECommerceCheckout'] as $googleItem){?>
+                                    {
+                                        <?=$googleItem?>
+                                    },
+                                    <?}?>
+                            ]
+                        }
+                    }
+                });
 
-				{ event: "setSiteType", type: "d" },
-				<?if($USER->IsAuthorized()){?>   
-					{ event: "setEmail", email: "<?=$USER->GetEmail()?>" },
-				<?} else {?>				
-					{ event: "setEmail", email: "" },
-				<?}?>
-				{ event: "viewBasket", item: [
-					<?foreach($_SESSION['itemsForCriteo'] as $criteoItem){?>
-						{
-							<?=$criteoItem?>
-						},
-					<?}?>					
-				]});
-			</script>
+            </script>
+
+            <script type="text/javascript" src="//static.criteo.net/js/ld/ld.js" async="true"></script>
+
+            <script type="text/javascript">
+                window.criteo_q = window.criteo_q || [];
+                window.criteo_q.push(
+                    { event: "setAccount", account: 18519 },
+
+                    { event: "setSiteType", type: "d" },
+                    <?if($USER->IsAuthorized()){?>   
+                        { event: "setEmail", email: "<?=$USER->GetEmail()?>" },
+                        <?} else {?>                
+                        { event: "setEmail", email: "" },
+                        <?}?>
+                    { event: "viewBasket", item: [
+                        <?foreach($_SESSION['itemsForCriteo'] as $criteoItem){?>
+                            {
+                                <?=$criteoItem?>
+                            },
+                            <?}?>                    
+                ]});
+            </script>
 
 
             <?
@@ -385,7 +343,7 @@
                                 }
                                 arshow($WishFilter);*/
                                 $uID = $USER -> GetID();
-                                
+
                                 if ($_REQUEST["list"]) {
                                     $list_user = CUser::GetByID($_REQUEST["list"]) -> Fetch();
                                     $list_user_name = $list_user["NAME"]." ".$list_user["LAST_NAME"];
@@ -394,10 +352,10 @@
                                     $list_user = CUser::GetByID($uID) -> Fetch();
                                     $list_user_name = $list_user["NAME"]." ".$list_user["LAST_NAME"];   
                                 }
-                                
+
                                 global $WishFilter;
                                 $WishFilter["NAME"] = $list_user_name;
-                                
+
                                 $APPLICATION->IncludeComponent(
                                     "bitrix:catalog.section", 
                                     "wish_list", 
@@ -529,31 +487,75 @@
             </form>
             <?
             }
+
+            if($arParams['USE_GIFTS'] == 'Y')
+            {   
+                $APPLICATION->IncludeComponent(
+                    "bitrix:sale.gift.basket",
+                    "basket_gifts",
+                    array(
+                        "SHOW_PRICE_COUNT" => 1,
+                        "PRODUCT_SUBSCRIPTION" => 'N',
+                        'PRODUCT_ID_VARIABLE' => 'id',
+                        "PARTIAL_PRODUCT_PROPERTIES" => 'N',
+                        "USE_PRODUCT_QUANTITY" => 'N',
+                        "ACTION_VARIABLE" => "actionGift",
+                        "ADD_PROPERTIES_TO_BASKET" => "Y",
+
+                        "BASKET_URL" => $APPLICATION->GetCurPage(),
+                        "APPLIED_DISCOUNT_LIST" => $arResult["APPLIED_DISCOUNT_LIST"],
+                        "FULL_DISCOUNT_LIST" => $arResult["FULL_DISCOUNT_LIST"],
+
+                        "TEMPLATE_THEME" => $arParams["TEMPLATE_THEME"],
+                        "PRICE_VAT_INCLUDE" => $arParams["PRICE_VAT_SHOW_VALUE"],
+                        "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+
+                        'BLOCK_TITLE' => $arParams['GIFTS_BLOCK_TITLE'],
+                        'HIDE_BLOCK_TITLE' => $arParams['GIFTS_HIDE_BLOCK_TITLE'],
+                        'TEXT_LABEL_GIFT' => $arParams['GIFTS_TEXT_LABEL_GIFT'],
+                        'PRODUCT_QUANTITY_VARIABLE' => $arParams['GIFTS_PRODUCT_QUANTITY_VARIABLE'],
+                        'PRODUCT_PROPS_VARIABLE' => $arParams['GIFTS_PRODUCT_PROPS_VARIABLE'],
+                        'SHOW_OLD_PRICE' => $arParams['GIFTS_SHOW_OLD_PRICE'],
+                        'SHOW_DISCOUNT_PERCENT' => $arParams['GIFTS_SHOW_DISCOUNT_PERCENT'],
+                        'SHOW_NAME' => $arParams['GIFTS_SHOW_NAME'],
+                        'SHOW_IMAGE' => $arParams['GIFTS_SHOW_IMAGE'],
+                        'MESS_BTN_BUY' => $arParams['GIFTS_MESS_BTN_BUY'],
+                        'MESS_BTN_DETAIL' => $arParams['GIFTS_MESS_BTN_DETAIL'],
+                        'PAGE_ELEMENT_COUNT' => $arParams['GIFTS_PAGE_ELEMENT_COUNT'],
+                        'CONVERT_CURRENCY' => $arParams['GIFTS_CONVERT_CURRENCY'],
+                        'HIDE_NOT_AVAILABLE' => $arParams['GIFTS_HIDE_NOT_AVAILABLE'],
+
+                        "LINE_ELEMENT_COUNT" => $arParams['GIFTS_PAGE_ELEMENT_COUNT'],
+                    ),
+                    false
+                );
+            }
+
         ?>
 
 
     </div>
 </div>
 <script>
-$(document).ready(function(){
-    
-    <?
-    if ($_REQUEST["action"])
-    {
-    ?>  
-        $(".cartMenuWrap .basketItems:first-child").removeClass("active");
-        $('.cartMenuWrap .basketItems:nth-child(2)').addClass("active");
-        $("#cardBlock2").show();
-    <?
-    }
-    else if (!$_REQUEST["liked"])
-    {
-    ?> 
-        $('.cartMenuWrap .basketItems:nth-child(2)').removeClass("active");
-        $('.cartMenuWrap .basketItems:first-child').addClass("active");
-        $("#cardBlock1").show();
-    <?
-    }
-    ?>   
-})
+    $(document).ready(function(){
+
+        <?
+            if ($_REQUEST["action"])
+            {
+            ?>  
+            $(".cartMenuWrap .basketItems:first-child").removeClass("active");
+            $('.cartMenuWrap .basketItems:nth-child(2)').addClass("active");
+            $("#cardBlock2").show();
+            <?
+            }
+            else if (!$_REQUEST["liked"])
+            {
+            ?> 
+            $('.cartMenuWrap .basketItems:nth-child(2)').removeClass("active");
+            $('.cartMenuWrap .basketItems:first-child').addClass("active");
+            $("#cardBlock1").show();
+            <?
+            }
+        ?>   
+    })
 </script>
