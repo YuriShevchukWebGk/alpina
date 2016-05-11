534a04d8e0c64f53e058c3b7a93f39cbbc4ce14d
Ruslik <br@webgk.ru>
2016-05-11 18:41:12 +0300

    Making 'series banners' functional

--- local/templates/.default/components/bitrix/catalog.section/bestsellers/template.php
+++ local/templates/.default/components/bitrix/catalog.section/bestsellers/template.php
@@ -66,7 +66,72 @@
                 <p class="text"><?=$arResult["SERIES"]["ELEMENT"]["DETAIL_TEXT"]?></p>
             </div>
             <?}?>
-
+            <? global $SeriesRoundBanner;
+            $SeriesRoundBanner = array("PROPERTY_BIND_TO_SERIE" => $arResult["SERIES"]["ID"]);
+            $APPLICATION->IncludeComponent(
+                "bitrix:news.list", 
+                "series_banners", 
+                array(
+                    "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                    "ADD_SECTIONS_CHAIN" => "Y",
+                    "AJAX_MODE" => "N",
+                    "AJAX_OPTION_ADDITIONAL" => "",
+                    "AJAX_OPTION_HISTORY" => "N",
+                    "AJAX_OPTION_JUMP" => "N",
+                    "AJAX_OPTION_STYLE" => "Y",
+                    "CACHE_FILTER" => "N",
+                    "CACHE_GROUPS" => "N",
+                    "CACHE_TIME" => "36000000",
+                    "CACHE_TYPE" => "A",
+                    "CHECK_DATES" => "Y",
+                    "COMPONENT_TEMPLATE" => "section_banners",
+                    "DETAIL_URL" => "",
+                    "DISPLAY_BOTTOM_PAGER" => "Y",
+                    "DISPLAY_DATE" => "Y",
+                    "DISPLAY_NAME" => "Y",
+                    "DISPLAY_PICTURE" => "Y",
+                    "DISPLAY_PREVIEW_TEXT" => "Y",
+                    "DISPLAY_TOP_PAGER" => "N",
+                    "FIELD_CODE" => array(
+                        0 => "DETAIL_PICTURE",
+                        1 => "",
+                    ),
+                    "FILTER_NAME" => "SeriesRoundBanner",
+                    "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                    "IBLOCK_ID" => "53",
+                    "IBLOCK_TYPE" => "news",
+                    "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+                    "INCLUDE_SUBSECTIONS" => "Y",
+                    "MESSAGE_404" => "",
+                    "NEWS_COUNT" => "20",
+                    "PAGER_BASE_LINK_ENABLE" => "N",
+                    "PAGER_DESC_NUMBERING" => "N",
+                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                    "PAGER_SHOW_ALL" => "N",
+                    "PAGER_SHOW_ALWAYS" => "N",
+                    "PAGER_TEMPLATE" => ".default",
+                    "PAGER_TITLE" => "Íîâîñòè",
+                    "PARENT_SECTION" => "",
+                    "PARENT_SECTION_CODE" => "",
+                    "PREVIEW_TRUNCATE_LEN" => "",
+                    "PROPERTY_CODE" => array(
+                        0 => "SERIE_BANNER_LINK",
+                        1 => "",
+                    ),
+                    "SET_BROWSER_TITLE" => "Y",
+                    "SET_LAST_MODIFIED" => "N",
+                    "SET_META_DESCRIPTION" => "Y",
+                    "SET_META_KEYWORDS" => "Y",
+                    "SET_STATUS_404" => "N",
+                    "SET_TITLE" => "Y",
+                    "SHOW_404" => "N",
+                    "SORT_BY1" => "ACTIVE_FROM",
+                    "SORT_BY2" => "SORT",
+                    "SORT_ORDER1" => "DESC",
+                    "SORT_ORDER2" => "ASC"
+                ),
+                false
+            );?>
              <?// Ð±Ð»Ð¾Ðº Ñ Ñ†Ð¸Ñ‚Ð°Ñ‚Ð¾Ð¹ END?>
             <ul class="filterParams">
                 <li <?if ($_REQUEST['SORT'] == 'POPULARITY' || !($_REQUEST['SORT'])){?>class="active"<?}?>><p data-id="1">
---
+++ local/templates/.default/components/bitrix/news.list/series_banners/template.php
@@ -0,0 +1,154 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+/*?>
+<div class="news-list">
+<?if($arParams["DISPLAY_TOP_PAGER"]):?>
+    <?=$arResult["NAV_STRING"]?><br />
+<?endif;?>
+<?foreach($arResult["ITEMS"] as $arItem):?>
+    <?
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    ?>
+    <p class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+        <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+            <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+                <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
+                        class="preview_picture"
+                        border="0"
+                        src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                        width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+                        height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+                        alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                        title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+                        style="float:left"
+                        /></a>
+            <?else:?>
+                <img
+                    class="preview_picture"
+                    border="0"
+                    src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                    width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+                    height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+                    alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                    title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+                    style="float:left"
+                    />
+            <?endif;?>
+        <?endif?>
+        <?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
+            <span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
+        <?endif?>
+        <?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
+            <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+                <a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a><br />
+            <?else:?>
+                <b><?echo $arItem["NAME"]?></b><br />
+            <?endif;?>
+        <?endif;?>
+        <?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
+            <?echo $arItem["PREVIEW_TEXT"];?>
+        <?endif;?>
+        <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+            <div style="clear:both"></div>
+        <?endif?>
+        <?foreach($arItem["FIELDS"] as $code=>$value):?>
+            <small>
+            <?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
+            </small><br />
+        <?endforeach;?>
+        <?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
+            <small>
+            <?=$arProperty["NAME"]?>:&nbsp;
+            <?if(is_array($arProperty["DISPLAY_VALUE"])):?>
+                <?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
+            <?else:?>
+                <?=$arProperty["DISPLAY_VALUE"];?>
+            <?endif?>
+            </small><br />
+        <?endforeach;?>
+    </p>
+<?endforeach;?>
+<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+    <br /><?=$arResult["NAV_STRING"]?>
+<?endif;?>
+</div><?/*?>
+<div class="slideWrapp">
+    <ul class="roundSlider">
+        <?foreach($arResult["ITEMS"] as $key => $arItem)
+        {?>
+        <li class="firstSlide" style="background-image: url(<?=$arItem["DETAIL_PICTURE"]["SRC"]?>)">
+        <div class="catalogWrapper">
+            <p class="titleSlide">
+            <?=$arItem["PREVIEW_TEXT"]?>
+            </p>
+            <p class="textSlide">
+            <?=$arItem["DETAIL_TEXT"]?>
+            </p>
+            <?foreach($arResult["ITEMS"] as $item_key => $item_value)
+            {?>
+            <!-- определение текущего слайда и присвоение класса к соответствующей кнопке слайда с соответствующей анимацией -->
+            <div class="<?if ($item_key==$key){?>circle circle<?=($item_key+1)?><?}else{?>buttons" data-number="<?=($item_key+1)?><?}?>">
+            <?if($item_key==$key) {?><strong><?=($item_key+1)?></strong><?} else {?><p><?=($item_key+1)?></p><?}?>
+            </div>
+            <?}?>
+        </div>
+        </li>
+        <?}?>
+    </ul>
+</div><?*/?>
+
+<? 
+    if($arResult['ELEMENTS']){?>
+        <div class="roundSlideWrapp">
+                    <ul class="roundSlider">
+                        <?foreach($arResult["ITEMS"] as $key => $arItem)
+                        {?>
+                        <li class="firstSlide">
+                            <?if ($arItem["PROPERTIES"]["SERIE_BANNER_LINK"]["VALUE"]){?>
+                                <a href="<?=$arItem["PROPERTIES"]["SERIE_BANNER_LINK"]["VALUE"]?>">
+                                <?}?>
+                                    <div class="catalogWrapper">
+                                        <img src="<?=$arItem["DETAIL_PICTURE"]["SRC"]?>" class="roundCatBack">
+                                        <p class="titleSlide"><?=$arItem["PREVIEW_TEXT"]?></p>
+                                        <p class="textSlide"><?=$arItem["DETAIL_TEXT"]?></p>
+                                        <?foreach($arResult["ITEMS"] as $item_key => $item_value)
+                                        {?>
+                                        <!-- определение текущего слайда и присвоение класса к соответствующей кнопке слайда с соответствующей анимацией -->
+                                        <div class="<?if ($item_key==$key){?>circle circle<?=($item_key+1)?><?}else{?>buttons" data-number="<?=($item_key+1)?><?}?>">
+                                        <?if($item_key==$key) {?><strong><?=($item_key+1)?></strong><?} else {?><p><?=($item_key+1)?></p><?}?>
+                                        </div>
+                                        <?}?>
+                                    </div>
+                                <?if ($arItem["PROPERTIES"]["SERIE_BANNER_LINK"]["VALUE"]){?>
+                                    </a>
+                                <?}?>
+                        </li>
+                        <?}?>
+                    </ul>    
+                </div>    
+    <?}
+?>
+<script>
+    $(document).ready(function(){
+        if($('.roundSlideWrapp').length == 0){
+            $('.categoryWrapper .titleMain').css({"margin-bottom":"-42px"});
+             $(".wrapperCategor").css("height", $(".wrapperCategor").height() - 360 + "px");
+             $(".contentWrapp").css("height", $(".contentWrapp").height() - 360 + "px");
+        }
+    })
+</script>
+
+
+          
