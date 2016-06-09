8c44e92d4fb7fcfb4226796d01d4e4cd86577cdd
Ruslik <br@webgk.ru>
2016-06-09 14:28:31 +0300

    Fixing code in cycle of catalog element's template

--- local/templates/.default/components/bitrix/catalog/catalog_template/bitrix/catalog.element/.default/template.php
+++ local/templates/.default/components/bitrix/catalog/catalog_template/bitrix/catalog.element/.default/template.php
@@ -89,22 +89,19 @@ $arItemIDs = array(
         <?
         $arBasketItems = array();
         $dbBasketItems = CSaleBasket::GetList(
-                      array("NAME" => "ASC","ID" => "ASC"),
-                      array("FUSER_ID" => CSaleBasket::GetBasketUserID(), "LID" => SITE_ID, "ORDER_ID" => "NULL"),
-                      false,
-                      false,
-                      array("ID","MODULE","PRODUCT_ID","QUANTITY","CAN_BUY","PRICE"));
+            array("NAME" => "ASC","ID" => "ASC"),
+            array("FUSER_ID" => CSaleBasket::GetBasketUserID(), "LID" => SITE_ID, "ORDER_ID" => "NULL"),
+            false,
+            false,
+            array("ID","MODULE","PRODUCT_ID","QUANTITY","CAN_BUY","PRICE")
+        );
+        $cart_num = 0;
+        $cart_sum = 0;
         while ($arItems = $dbBasketItems->Fetch()) {
-          $arItems = CSaleBasket::GetByID($arItems["ID"]);
-          $arBasketItems[] = $arItems;
-          $cart_num += $arItems['QUANTITY'];
-          $cart_sum += $arItems['PRICE'] * $arItems['QUANTITY'];
-        }
-        if (empty($cart_num)) {
-            $cart_num = "0";
-        }
-        if (empty($cart_sum)) {
-            $cart_sum = "0";
+            $arItems = CSaleBasket::GetByID($arItems["ID"]);
+            $arBasketItems[] = $arItems;
+            $cart_num += $arItems['QUANTITY'];
+            $cart_sum += $arItems['PRICE'] * $arItems['QUANTITY'];
         }
         ?>
         <div class="elementDescriptWrap">
