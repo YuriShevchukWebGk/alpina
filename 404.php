ef58ae13a877e5a3345263dd13004a95e0de8998
Admin <git@webgk.ru>
2016-06-15 12:51:14 +0300

    work copy updates

--- 404.php
+++ 404.php
@@ -8,16 +8,12 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("Страница не найдена");?>
 
-    <div class="bx-404-container">
-        <div class="bx-404-block"><img src="<?=SITE_DIR?>images/404.png" alt=""></div>
-        <div class="bx-404-text-block">Неправильно набран адрес, <br>или такой страницы на сайте больше не существует.</div>
-        <div class="">Вернитесь на <a href="<?=SITE_DIR?>">главную</a> или воспользуйтесь картой сайта.</div>
-    </div>
-    <div class="map-columns row">
-        <div class="col-sm-10 col-sm-offset-1">
-            <div class="bx-maps-title">Карта сайта:</div>
+        <div class="noResultBodyWrap">
+            <div class="centerWrapper noResWrapp">
+                <p class="noResultTitle">Неправильно набран адрес, <br>или такой страницы на сайте больше не существует.</p>
+                <p class="noResText">Вернитесь на <a href="<?=SITE_DIR?>">главную</a> или воспользуйтесь картой сайта.</p>
+            </div>
         </div>
-    </div>
 
     <div class="col-sm-offset-2 col-sm-4">
         <div class="bx-map-title"><i class="fa fa-leanpub"></i> Каталог</div>
--- local/templates/.default/components/bitrix/catalog/catalog_template/bitrix/catalog.element/.default/template.php
+++ local/templates/.default/components/bitrix/catalog/catalog_template/bitrix/catalog.element/.default/template.php
@@ -507,7 +507,7 @@ $arItemIDs = array(
 						}
 					} else {
 						if ($today == 1) {
-							$delivery_day = 'в&nbsp;среду';
+							$delivery_day = 'завтра';
 						} elseif ($today == 2) {
 							$delivery_day = 'завтра';
 						} elseif ($today == 3) {
@@ -515,16 +515,16 @@ $arItemIDs = array(
 						} elseif ($today == 4) {
 							$delivery_day = 'завтра';
 						} elseif ($today == 5) {
-							$delivery_day = 'во&nbsp;вторник';
+							$delivery_day = 'в&nbsp;понедельник';
 						} elseif ($today == 6) {
-							$delivery_day = 'во&nbsp;вторник';
+							$delivery_day = 'в&nbsp;понедельник';
 						} elseif ($today == 0) {
-							$delivery_day = 'во&nbsp;вторник';
+							$delivery_day = 'завтра';
 						}
 						if ($timenow < 17 && $today != 6) {
 							$samovivoz_day = 'сегодня';
 						} else {
-							$samovivoz_day = 'во вторник';
+							$samovivoz_day = 'завтра';
 						}
 
 					}?>
