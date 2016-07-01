8d28dc860301cfda0039f961ec7b5bf17b98e8bb
Ruslik <br@webgk.ru>
2016-07-01 12:50:34 +0300

    SUP-4622: removing test.php

--- test.php
+++
@@ -1,5 +0,0 @@
-<?
-    require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-    getDeliveryDate("65588");
-?>                                                          
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
