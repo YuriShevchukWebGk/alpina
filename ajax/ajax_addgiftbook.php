26a815cbe1e9da8f53345ce34ef08a08c65984ab
Ruslik <br@webgk.ru>
2016-07-18 18:00:36 +0300

    SUP-4210: fixing codestyle

--- ajax/ajax_addgiftbook.php
+++ ajax/ajax_addgiftbook.php
@@ -1,15 +1,15 @@
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+<?require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
     CModule::IncludeModule("sale"); CModule::IncludeModule("catalog"); CModule::IncludeModule("iblock");
 ?>
 <?  
-if(intval($_REQUEST["productid"]) > 0 && intval($_REQUEST["quantity"]) > 0){
+if (intval($_REQUEST["productid"]) > 0 && intval($_REQUEST["quantity"]) > 0){
 
     global $USER;
-    $quantity = $_REQUEST["quantity"];    
+    $quantity = intval($_REQUEST["quantity"]);    
     $product = intval($_REQUEST["productid"]);
 
     $res = CIBlockElement::GetByID($product);
-    if($ar_res = $res->GetNext()) {
+    if ($ar_res = $res->GetNext()) {
 
         // получаем цену соответствующего типа для подвешенного товара
         $ar_price = CPrice::GetList(
@@ -23,8 +23,8 @@ if(intval($_REQUEST["productid"]) > 0 && intval($_REQUEST["quantity"]) > 0){
             array()
         ) -> Fetch();
         $price = $ar_price["PRICE"];
-        if(intval($price) > 0){
-            $arFields = array(
+        if (intval($price) > 0){
+            $ar_fields = array(
                 "LID" => "s1",
                 "PERSON_TYPE_ID" => "1",
                 "PAYED" => "N",
@@ -40,12 +40,12 @@ if(intval($_REQUEST["productid"]) > 0 && intval($_REQUEST["quantity"]) > 0){
                 "TAX_VALUE" => 0.0,
                 "USER_DESCRIPTION" => $_REQUEST["name"]
             );
-            
+
             // создаём заказ и добавляем туда запрошенный подвешенный товар
-            
-            if ($ORDER_ID = CSaleOrder::Add($arFields)) {
-                $ORDER_ID = intval($ORDER_ID);
-                $itemFields = array(
+
+            if ($order_id = CSaleOrder::Add($ar_fields)) {
+                $order_id = intval($order_id);
+                $item_fields = array(
                     "FUSER_ID" => CSaleBasket::GetBasketUserID(),
                     "PRODUCT_ID" => $product,
                     "PRODUCT_PRICE_ID" => SUSPENDED_BOOKS_PRICE_ID,
@@ -58,9 +58,9 @@ if(intval($_REQUEST["productid"]) > 0 && intval($_REQUEST["quantity"]) > 0){
                     "PRODUCT_PROVIDER_CLASS" => "CCatalogProductProvider",
                     "MODULE" => "catalog"
                 );
-                $gift_item = CSaleBasket::Add($itemFields);
-                CSaleBasket::OrderBasket($ORDER_ID, 0, SITE_ID, false);
-                echo $ORDER_ID;
+                $gift_item = CSaleBasket::Add($item_fields);
+                CSaleBasket::OrderBasket($order_id, 0, SITE_ID, false);
+                echo $order_id;
             } else {
                 CSaleBasket::DeleteAll(CSaleBasket::GetBasketUserID());
                 echo "err";
