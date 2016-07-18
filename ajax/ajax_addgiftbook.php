be5f2173e4bcc80f8daf62eb70e250b8e1398645
Ruslik <br@webgk.ru>
2016-07-18 17:40:12 +0300

    SUP-4210: fixing codestyle

--- ajax/ajax_addgiftbook.php
+++ ajax/ajax_addgiftbook.php
@@ -2,72 +2,73 @@
     CModule::IncludeModule("sale"); CModule::IncludeModule("catalog"); CModule::IncludeModule("iblock");
 ?>
 <?  
-    
+if(intval($_REQUEST["productid"]) > 0 && intval($_REQUEST["quantity"]) > 0){
 
-    if(intval($_REQUEST["productid"]) > 0){
+    global $USER;
+    $quantity = $_REQUEST["quantity"];    
+    $product = intval($_REQUEST["productid"]);
 
-        $quantity = $_REQUEST["quantity"];
-        
-        $product = intval($_REQUEST["productid"]);
+    $res = CIBlockElement::GetByID($product);
+    if($ar_res = $res->GetNext()) {
 
-             
-        $res = CIBlockElement::GetByID($product);
-        if($ar_res = $res->GetNext()) {
-            $arProps = array();
-            $PRODUCT = $ar_res;
-
-            $ar_res = CPrice::GetList(array(), array("PRODUCT_ID" => $PRODUCT["ID"], "CATALOG_GROUP_ID" => SUSPENDED_BOOKS_PRICE_ID), false, false, array()) -> Fetch();
-            $price = $ar_res["PRICE"];
-            if(intval($price) > 0){
-                $arFields = array(
+        // получаем цену соответствующего типа для подвешенного товара
+        $ar_price = CPrice::GetList(
+            array(), 
+            array(
+                "PRODUCT_ID" => $ar_res["ID"], 
+                "CATALOG_GROUP_ID" => SUSPENDED_BOOKS_PRICE_ID
+            ), 
+            false, 
+            false, 
+            array()
+        ) -> Fetch();
+        $price = $ar_price["PRICE"];
+        if(intval($price) > 0){
+            $arFields = array(
+                "LID" => "s1",
+                "PERSON_TYPE_ID" => "1",
+                "PAYED" => "N",
+                "CANCELED" => "N",
+                "STATUS_ID" => "N",
+                "PRICE" => 0,
+                "CURRENCY" => "RUB",
+                "USER_ID" => $USER -> GetID(),
+                "PAY_SYSTEM_ID" => RFI_PAYSYSTEM_ID,
+                "PRICE_DELIVERY" => 0,
+                "DELIVERY_ID" => PICKUP_DELIVERY_ID,
+                "DISCOUNT_VALUE" => 0,
+                "TAX_VALUE" => 0.0,
+                "USER_DESCRIPTION" => $_REQUEST["name"]
+            );
+            
+            // создаём заказ и добавляем туда запрошенный подвешенный товар
+            
+            if ($ORDER_ID = CSaleOrder::Add($arFields)) {
+                $ORDER_ID = intval($ORDER_ID);
+                $itemFields = array(
+                    "FUSER_ID" => CSaleBasket::GetBasketUserID(),
+                    "PRODUCT_ID" => $product,
+                    "PRODUCT_PRICE_ID" => SUSPENDED_BOOKS_PRICE_ID,
+                    "PRICE" => $price,
+                    "CURRENCY" => 'RUB',
+                    "QUANTITY" => $quantity,
+                    "PRODUCT_XML_ID" => $ar_res["ID"],
                     "LID" => "s1",
-                    "PERSON_TYPE_ID" => "1",
-                    "PAYED" => "N",
-                    "CANCELED" => "N",
-                    "STATUS_ID" => "N",
-                    "PRICE" => 0,
-                    "CURRENCY" => "RUB",
-                    "USER_ID" => $USER -> GetID(),
-                    "PAY_SYSTEM_ID" => RFI_PAYSYSTEM_ID,
-                    "PRICE_DELIVERY" => 0,
-                    "DELIVERY_ID" => PICKUP_DELIVERY_ID,
-                    "DISCOUNT_VALUE" => 0,
-                    "TAX_VALUE" => 0.0,
-                    "USER_DESCRIPTION" => $_REQUEST["name"]
+                    "NAME" => $ar_res["NAME"],
+                    "PRODUCT_PROVIDER_CLASS" => "CCatalogProductProvider",
+                    "MODULE" => "catalog"
                 );
-                if ($ORDER_ID = CSaleOrder::Add($arFields)) {
-                    $ORDER_ID = intval($ORDER_ID);
-                    $itemFields = array(
-                        "FUSER_ID" => CSaleBasket::GetBasketUserID(),
-                        "PRODUCT_ID" => $product,
-                        "PRODUCT_PRICE_ID" => SUSPENDED_BOOKS_PRICE_ID,
-                        "PRICE" => $price,
-                        "CURRENCY" => 'RUB',
-                        "QUANTITY" => $quantity,
-                        "PRODUCT_XML_ID" => $PRODUCT["ID"],
-                        "LID" => "s1",
-                        "NAME" => $PRODUCT["NAME"],
-                        "PRODUCT_PROVIDER_CLASS" => "CCatalogProductProvider",
-                        "MODULE" => "catalog"
-                    );
-                    $gift_item = CSaleBasket::Add($itemFields);
-                    CSaleBasket::OrderBasket($ORDER_ID, 0, SITE_ID, false);
-                    echo $ORDER_ID;
-                } else {
-                    CSaleBasket::DeleteAll(CSaleBasket::GetBasketUserID());
-                    echo "err";
-                }
-
-
+                $gift_item = CSaleBasket::Add($itemFields);
+                CSaleBasket::OrderBasket($ORDER_ID, 0, SITE_ID, false);
+                echo $ORDER_ID;
             } else {
+                CSaleBasket::DeleteAll(CSaleBasket::GetBasketUserID());
                 echo "err";
             }
 
+        } else {
+            echo "err";
         }
-
     }
-            
-
-    
-   
+}
 ?>
--- ajax/making_buyers_list.php
+++ ajax/making_buyers_list.php
@@ -2,38 +2,32 @@
     CModule::IncludeModule("sale"); CModule::IncludeModule("catalog"); CModule::IncludeModule("iblock");
 ?>
 <?  
-    $buyers_list = "";
-
-    if(intval($_REQUEST["item_id"]) > 0){
-
-        $buyers_list .= " <table>
-            <tr>
-                <td>Инициалы</td>
-                <td>Кол-во, шт.</td>
-            </tr>";
-        $gift_book_buyers = CIBlockElement::GetList (
-            array(), 
-            array(
-                "IBLOCK_ID" => SUSPENDED_BOOKS_BUYERS_IBLOCK, "PROPERTY_GIFT_BOOK" => $_REQUEST["item_id"]
-            ), 
-            false, 
-            false, 
-            array("NAME", "ID", "PROPERTY_GIFT_BOOK", "PROPERTY_GIFT_QUANTITY")
-        );
+$buyers_list = "";
+if(intval($_REQUEST["item_id"]) > 0){ 
+    $buyers_list .= " <table>
+    <tr>
+    <td>Инициалы</td>
+    <td>Кол-во, шт.</td>
+    </tr>";
+    
+    // получаем список купивших данную книгу в дар из соответствующего инфоблока
+    $gift_book_buyers = CIBlockElement::GetList (
+        array(), 
+        array(
+            "IBLOCK_ID" => SUSPENDED_BOOKS_BUYERS_IBLOCK, "PROPERTY_GIFT_BOOK" => $_REQUEST["item_id"]
+        ), 
+        false, 
+        false, 
+        array("NAME", "ID", "PROPERTY_GIFT_BOOK", "PROPERTY_GIFT_QUANTITY")
+    );
+    if (intval($gift_book_buyers -> SelectedRowsCount()) > 0) {
         while ($buyers = $gift_book_buyers -> Fetch()) {
-            if (intval($gift_book_buyers -> SelectedRowsCount()) > 0) {
-                $buyers_list .=  "<tr><td>" . $buyers["NAME"] . "</td><td><div class='rounded_number'>" . intval($buyers["PROPERTY_GIFT_QUANTITY_VALUE"]) . "</div></td></tr>";
-            } else {
-                $buyers_list .= "<tr><td width='99%'>Данную книгу пока никто не купил в дар.</td><td></td>";
-            }  
+            $buyers_list .=  "<tr><td>" . $buyers["NAME"] . "</td><td><div class='rounded_number'>" . intval($buyers["PROPERTY_GIFT_QUANTITY_VALUE"]) . "</div></td></tr>";  
         }
-        $buyers_list .= "</table>";
-
-    };
-    
-    echo $buyers_list;
-            
-
-    
-   
+    } else {
+        $buyers_list .= "<tr><td width='99%'>Данную книгу пока никто не купил в дар.</td><td></td>";
+    }
+    $buyers_list .= "</table>";
+};
+echo $buyers_list;
 ?>
