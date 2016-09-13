ed893a89e3dce4efd86e66cd82b9217470317044
Ruslik <br@webgk.ru>
2016-09-13 12:17:30 +0300

    Updates from previous ticket

--- local/templates/alpina_basket/header.php
+++ local/templates/alpina_basket/header.php
@@ -98,7 +98,7 @@
 		'userRegCategory' : 'UserUnregistered'
 	}];
 	</script>
-<?}?>
+<?}/*?>
 <!-- Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PM87GH"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
@@ -107,7 +107,7 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PM87GH');</script>
-<!-- End Google Tag Manager -->
+<!-- End Google Tag Manager --> <?*/?>
 <div id="panel"><?$APPLICATION->ShowPanel();?></div>
 
 <div class="basketHeader">
