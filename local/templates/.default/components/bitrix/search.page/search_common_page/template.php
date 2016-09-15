4c955c357cfaebb4bb411ccc2934e3c638364585
Mihan <lm@webgk.ru>
2016-09-15 12:05:48 +0300

    SUP-5002: Author name source changed

--- local/templates/.default/components/bitrix/search.page/search_common_page/template.php
+++ local/templates/.default/components/bitrix/search.page/search_common_page/template.php
@@ -264,7 +264,7 @@
                                 <div class="descrWrap">
                                     <a href="<?= $arItem["URL"] ?>">
                                         <p class="bookNames" title="<?= $arItem["TITLE"] ?>"><?= $arItem["TITLE"] ?></p>
-                                        <p class="autorName"><?= $arResult["BOOK_AUTHOR_INFO"][$arResult["BOOK_INFO"][$arItem["ITEM_ID"]]["PROPERTY_AUTHORS_VALUE"]]["NAME"] ?></p>
+                                        <p class="autorName"><?= $arResult["BOOK_AUTHOR_INFO"][$arResult["BOOK_INFO"][$arItem["ITEM_ID"]]["PROPERTY_AUTHORS_VALUE"]] ?></p>
                                         <p class="wrapperType"><?= $arResult["BOOK_INFO"][$arItem["ITEM_ID"]]["PROPERTY_COVER_TYPE_VALUE"]?></p>
                                         <?if (($arResult["BOOK_INFO"][$arItem["ITEM_ID"]]["PROPERTY_STATE_ENUM_ID"] != getXMLIDByCode(CATALOG_IBLOCK_ID, "STATE", "soon"))
                                             && ($arResult["BOOK_INFO"][$arItem["ITEM_ID"]]["PROPERTY_STATE_ENUM_ID"] != getXMLIDByCode(CATALOG_IBLOCK_ID, "STATE", "net_v_nal"))) {
