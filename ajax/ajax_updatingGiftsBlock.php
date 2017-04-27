d09adc7b3807986b99072a1a88f431324ef284c2
Ruslik <br@webgk.ru>
2017-04-27 16:56:00 +0300

    Fixing ajax_updatingGiftsBlock.php

--- ajax/ajax_updatingGiftsBlock.php
+++ ajax/ajax_updatingGiftsBlock.php
@@ -1,4 +1,4 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 CModule::IncludeModule("sale"); CModule::IncludeModule("catalog"); CModule::IncludeModule("iblock");
-global $APPLICATION;
+
 $APPLICATION -> ShowViewContent('gifts_block');?>
