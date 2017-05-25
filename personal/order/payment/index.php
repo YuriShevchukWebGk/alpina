ff90a2c01bd595f4401605f62078071f31e8bf30
Admin <git@webgk.ru>
2017-05-25 12:58:22 +0300

    local changes 25.05.2017

--- personal/order/payment/index.php
+++ personal/order/payment/index.php
@@ -1,4 +1,4 @@
-<?
+<?                                                                                         
     require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
     $APPLICATION->SetTitle("Оплата заказа");
 ?>
