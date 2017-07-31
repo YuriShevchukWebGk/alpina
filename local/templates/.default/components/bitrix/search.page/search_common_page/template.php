ccd03b2741598387e2bcec1fd8534b866a339039
Alexander_Marchenkov <a.marchenkov@alpinabook.ru>
2017-07-31 12:12:32 +0300

    Discount fix

--- local/templates/.default/components/bitrix/search.page/search_common_page/template.php
+++ local/templates/.default/components/bitrix/search.page/search_common_page/template.php
@@ -256,13 +256,17 @@
                                // arshow($arResult["BOOK_INFO"][$arItem["ITEM_ID"]]["DISCOUNT_INFO"]["VALUE"]);
                                 //$newPrice = $newPrice * (1 - $item_discount_value / 100);
 								
-								if ($discount) {
+								/*if ($discount) {
 									$newPrice = round ($item_discount_value * (1 - $discount / 100), 2);
 									if (strlen (stristr($newPrice, ".")) == 2) {
 										$newPrice .= "0";
 									}
 								} else {
 									$newPrice = $item_discount_value;
+								}*/
+								$newPrice = round ($item_discount_value * (1 - $discount / 100), 2);
+								if (strlen (stristr($newPrice, ".")) == 2) {
+									$newPrice .= "0";
 								}
                             }
                             if($arResult["BOOK_INFO"][$arItem["ITEM_ID"]]["IBLOCK_SECTION_ID"] == CERTIFICATE_SECTION_ID) {
