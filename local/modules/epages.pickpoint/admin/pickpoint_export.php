0b18419cd8432da4768cd098fdcb272a327df5c6
Admin <git@webgk.ru>
2017-08-02 09:25:35 +0300

    local changes 02.08.2017

--- local/modules/epages.pickpoint/admin/pickpoint_export.php
+++ local/modules/epages.pickpoint/admin/pickpoint_export.php
@@ -151,7 +151,7 @@ if($_REQUEST["mess"] == "save")
 					<td><?=GetMessage("PP_RECEPTION_TYPE")?></td>
 <?/*					<td><?=GetMessage("PP_SIZE")?></td>               */?>
 
-				</tr>   
+				</tr>                
 				<?foreach($arItems as $arItem):?>  
 					<?$arRequestItem = $_REQUEST["EXPORT"][$arItem["ORDER_ID"]];
                         $bActive = $arItem["INVOICE_ID"]?false:true;
--- local/templates/.default/components/bitrix/sale.order.ajax/order/result_modifier.php
+++ local/templates/.default/components/bitrix/sale.order.ajax/order/result_modifier.php
@@ -22,19 +22,19 @@ foreach($arResult["DELIVERY"] as $DeliveryID => $DeliveryResult) {
 };                                                                                   
 /*global $USER;
 if ($USER->GetID() && $USER->IsAuthorized()) {
-	$users = CUser::GetList(
-		($by=""),
-		($order=""),
-		Array(
-			"ID" => $USER->GetID()
-		),
-		Array(
-			"SELECT" => Array("UF_RECURRENT_ID", "UF_RECURRENT_CARD_ID")
-		)
-	); 
-	if ($user = $users->NavNext(true, "f_")) {
-		$arResult["UF_RECURRENT_ID"] = $user["UF_RECURRENT_ID"];
-		$arResult["UF_RECURRENT_CARD_ID"] = $user["UF_RECURRENT_CARD_ID"];
-	}
+    $users = CUser::GetList(
+        ($by=""),
+        ($order=""),
+        Array(
+            "ID" => $USER->GetID()
+        ),
+        Array(
+            "SELECT" => Array("UF_RECURRENT_ID", "UF_RECURRENT_CARD_ID")
+        )
+    ); 
+    if ($user = $users->NavNext(true, "f_")) {
+        $arResult["UF_RECURRENT_ID"] = $user["UF_RECURRENT_ID"];
+        $arResult["UF_RECURRENT_CARD_ID"] = $user["UF_RECURRENT_CARD_ID"];
+    }
 }*/
 ?>
