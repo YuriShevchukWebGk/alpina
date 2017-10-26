3534dc18ed113beca05883a758e230170a4ef5c2
Admin <git@webgk.ru>
2017-10-26 14:20:28 +0300

    local changes 26.10.2017

--- local/templates/.default/components/bitrix/catalog.section/coming-soon/template.php
+++ local/templates/.default/components/bitrix/catalog.section/coming-soon/template.php
@@ -11,7 +11,7 @@
     /** @var string $componentPath */
     /** @var CBitrixComponent $component */
     $this->setFrameMode(true);
-	$gdeSlon = '';
+    $gdeSlon = '';
 ?>
 <?if ($_REQUEST["DIRECTION"] == "DESC")
     {?>
@@ -152,7 +152,7 @@
                                         <p class="priceOfBook"><?=strtolower(FormatDate("j F Y", MakeTimeStamp($arItem['PROPERTIES']['SOON_DATE_TIME']['VALUE'], "DD.MM.YYYY HH:MI:SS")));?></p>
                                     <?
                                     }
-										$gdeSlon .= $arItem['ID'].':'.ceil($arPrice["DISCOUNT_VALUE_VAT"]).',';
+                                        $gdeSlon .= $arItem['ID'].':'.ceil($arPrice["DISCOUNT_VALUE_VAT"]).',';
                                         if ($USER -> IsAuthorized())
                                         {
                                         ?>
