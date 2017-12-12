57cf63806fdda8d2cfc84aba839c2387c202bc77
Admin <git@webgk.ru>
2017-12-12 17:25:55 +0300

    SUP-7297: fixing conditions for deleting item from the basket

--- local/templates/.default/components/bitrix/sale.basket.basket/basket/basket_items.php
+++ local/templates/.default/components/bitrix/sale.basket.basket/basket/basket_items.php
@@ -134,7 +134,7 @@
                             $arItems = CSaleDiscount::GetByID(GIFT_BAG_EXHIBITION);
                             $count_number = preg_replace("/[^0-9]/", '', $arItems["UNPACK"]);  // вытаскиваем количество позиций из условия
 
-                            if($count < $count_number && !stristr($arItem["DETAIL_PAGE_URL"], "/sumki/") && ($arItem["PROPERTY_COVER_TYPE_VALUE"] == 'Подарок' || empty($arItem["PROPERTY_COVER_TYPE_VALUE"]))){
+                            if($count < $count_number && !stristr($arItem["DETAIL_PAGE_URL"], "/sumki/") && (/*$arItem["PROPERTY_COVER_TYPE_VALUE"] == 'Подарок' || */empty($arItem["PROPERTY_COVER_TYPE_VALUE"]))){
                                 CSaleBasket::Delete($arItem["ID"]);
                             }
                             $totalQuantity += $arItem["QUANTITY"];
