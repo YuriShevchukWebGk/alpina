e7354f7284edee224b36ce3e7f96db20b9b5d499
Admin <git@webgk.ru>
2018-01-12 12:56:33 +0300

    SUP-7355: edit popap city

--- local/templates/.default/components/bitrix/iblock.element.add.form/feedback_form/template.php
+++ local/templates/.default/components/bitrix/iblock.element.add.form/feedback_form/template.php
@@ -390,13 +390,7 @@ if (strlen($arResult["MESSAGE"]) > 0):?>
             $("input[name='PROPERTY[183][0]']").css("border", "1px solid #f0f0f0");    
         }
 		
-        if (!isTelephone($("input[name='PROPERTY[184][0]']").val())) {
-            e.preventDefault();
-            $("input[name='PROPERTY[184][0]']").css("border", "1px solid red");
-        } else {
-            $("input[name='PROPERTY[184][0]']").css("border", "1px solid #f0f0f0");    
-        }
-		
+
 		if ($("input[name='PROPERTY[NAME][0]']").val() == '') {
             e.preventDefault();
             $("input[name='PROPERTY[NAME][0]']").css("border", "1px solid red");
