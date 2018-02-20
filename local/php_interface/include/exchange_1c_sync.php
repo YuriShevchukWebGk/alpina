1072a73778fc4f46c0a179850913bb093c64820c
Ruslik <br@webgk.ru>
2018-02-20 17:04:55 +0300

    1c_sync fix

--- local/php_interface/include/exchange_1c_sync.php
+++ local/php_interface/include/exchange_1c_sync.php
@@ -69,44 +69,61 @@ class Exchange1C {
             $bitrix_id = $arResult['PROPERTY_ID_BITRIKS_VALUE'];
         }
 
+        logger('$bitrix_id:', $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+        logger($bitrix_id, $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
 
         if(!empty($bitrix_id)) {
+
+            logger('$bitrix_id_YES:', $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+
 
             //Запрос для получения остатков у элементов, которые привязаны к тому же товару в каталоге
             $arSelect = Array("CATALOG_QUANTITY", "NAME");
             $arFilter_1 = Array("IBLOCK_ID"=>IBLOCK_1C_EXCHANGE, "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y", "PROPERTY_ID_BITRIKS_VALUE" => $bitrix_id);
             $cat = CIBlockElement::GetList(Array(), $arFilter_1, false, false, $arSelect);
-         //   $total_quantity = 0;
+          //  $total_quantity = 0;
             while($arCatalog = $cat->Fetch()) {
                 $total_quantity = $total_quantity + $arCatalog['CATALOG_QUANTITY'];
-                $name = $arCatalog['NAME'];
+               // $name = $arCatalog['NAME'];
             }
 
-
-             $bitrix_id_elem_info = CIBlockElement::GetList (array(), array("IBLOCK_ID" => CATALOG_IBLOCK_ID, "ID" => $bitrix_id), false, false, array("CATALOG_QUANTITY", "CANONICAL_PAGE_URL"));
+             $bitrix_id_elem_info = CIBlockElement::GetList (array(), array("IBLOCK_ID" => CATALOG_IBLOCK_ID, "ID" => $bitrix_id), false, false, array("CATALOG_QUANTITY", "CANONICAL_PAGE_URL", "PROPERTY_STATE", "PROPERTY_reissue", "NAME"));
              while ($bitrix_id_elem = $bitrix_id_elem_info -> GetNext()) {
                   $quantity = $bitrix_id_elem["CATALOG_QUANTITY"];
                   $href = $bitrix_id_elem['CANONICAL_PAGE_URL'];
-             }
+                  $state = $bitrix_id_elem['PROPERTY_STATE_ENUM_ID'];
+                  $reissue = $bitrix_id_elem['PROPERTY_REISSUE_VALUE'];
+                  $name = $bitrix_id_elem['NAME'];
 
-             //logger($arFields, $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+                  logger($bitrix_id_elem, $_SERVER["DOCUMENT_ROOT"].'/logs/log_1.log');
+             }
 
             //Запросе на обновление остатков у товара
             $arField = array('QUANTITY' => $total_quantity);// зарезервированное количество
             if($total_quantity > 0 && $quantity <= 0){
+                if($state == STATE_SOON && $reissue == ""){
+                    CIBlockElement::SetPropertyValuesEx($bitrix_id, false, array("STATE" => STATE_NEWS));
+                } else if($state == STATE_SOON && $reissue == "Y"){
+                    CIBlockElement::SetPropertyValuesEx($bitrix_id, false, array("STATE" => ""));
+                } else if($state == STATE_NULL){
+                    CIBlockElement::SetPropertyValuesEx($bitrix_id, false, array("STATE" => ""));
+                }
                 $mailFields = array(   // В наличии
                     "QUANTITY"=> 'В НАЛИЧИИ',
                     "PAGE_URL" => $href,
                     "NAME" => $name,
                 );
-                CEvent::Send("LOW_QUANTITY", "s1", $mailFields, "N", 408);
-            } else if($total_quantity <= 0 && $quantity > 0){
+                CEvent::Send("BOOK_AVAILABILITY", "s1", $mailFields, "N");
+            } else if($total_quantity <= 0 && $quantity > 0 && $state != STATE_SOON){
+             //   if($state == "NULL" || $state == STATE_NEWS){
+                    CIBlockElement::SetPropertyValuesEx($bitrix_id, false, array("STATE" => STATE_NULL));
+             //   }
                 $mailFields = array(   // В наличии
                     "QUANTITY"=> 'НЕТ В НАЛИЧИИ',
                     "PAGE_URL" => $href,
                     "NAME" => $name,
                 );
-                CEvent::Send("LOW_QUANTITY", "s1", $mailFields, "N", 408);
+                CEvent::Send("BOOK_AVAILABILITY", "s1", $mailFields, "N");
             }
 
           //  if ($state_prop_enum_id != getXMLIDByCode (CATALOG_IBLOCK_ID, "STATE", "soon")) {
@@ -123,3 +140,114 @@ class Exchange1C {
 
     }
 }
+
+/*
+//РЎРёРЅС…СЂРѕРЅРёР·Р°С†РёСЏ РѕСЃС‚Р°С‚РєРѕРІ РїСЂРё РІС‹РіСЂСѓР·РєРµ РІ РёРЅС„РѕР±Р»РѕРє 1РЎ
+AddEventHandler("catalog", "OnProductUpdate", Array("Exchange1C", "SyncProductQuantity"));
+AddEventHandler("catalog", "OnProductAdd", Array("Exchange1C", "SyncProductQuantity"));
+
+//РЈРІРµРґРѕРјР»РµРЅРёРµ Рё СЃРјРµРЅР° СЃС‚Р°С‚СѓСЃР° СЃ 0 РѕСЃС‚Р°С‚РєР°С… РЅР° СЃР°Р№С‚Рµ
+AddEventHandler("catalog", "OnBeforeProductUpdate", Array("QuantityChanges", "QuantityOnZero"));
+
+//Р›РѕРіРёСЂРѕРІР°РЅРёРµ РёР·РјРµРЅРµРЅРёР№ СЌР»РµРјРµРЅС‚РѕРІ
+AddEventHandler("iblock", "OnAfterIBlockElementAdd", Array("Exchange1C", "SyncProductQuantityIblock"));
+AddEventHandler("iblock", "OnAfterIBlockElementUpdate", Array("Exchange1C", "SyncProductQuantityIblock"));
+
+class QuantityChanges {
+    function QuantityOnZero($ID, $arFields) {
+        if($arFields['QUANTITY'] <= 0 && CModule::IncludeModule('iblock')) {
+            $arSelect = Array("CATALOG_QUANTITY", "PROPERTY_STATE", "NAME");
+            $arFilter = Array("ID" => $ID, "IBLOCK_ID" => CATALOG_IBLOCK_ID, "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y");
+            $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
+            if ($ob = $res->GetNextElement()) {
+                $ar_product = $ob->GetFields();
+                //РџСЂРѕРІРµСЂРёРј РЅРµ СЏРІР»СЏРµС‚СЃСЏ Р»Рё РїСЂРµРґР·Р°РєР°Р·РѕРј Рё РЅРµ Р±С‹Р» Р»Рё СЂР°РЅРµРµ РѕС‚СЃС‚Р°С‚РѕРє РјРµРЅСЊС€Рµ 0, РїРѕСЃР»Рµ С‡РµРіРѕ РѕС‚РїСЂР°РІРёРј СЃРѕРѕР±С‰РµРЅРёРµ Рё РїРѕРјРµРЅСЏРµРј СЃС‚Р°С‚СѓСЃ
+                if(($ar_product['PROPERTY_STATE_ENUM_ID'] != getXMLIDByCode(CATALOG_IBLOCK_ID, "STATE", "soon")) && ($ar_product['CATALOG_QUANTITY'] <= 0)) {
+                    //РЈСЃС‚Р°РЅРѕРІРёРј РЅРѕРІРѕРµ Р·РЅР°С‡РµРЅРёРµ РґР»СЏ РґР°РЅРЅРѕРіРѕ СЃРІРѕР№СЃС‚РІР° РґР°РЅРЅРѕРіРѕ СЌР»РµРјРµРЅС‚Р°
+                    CIBlockElement::SetPropertyValuesEx($ID, false, array('STATE' => getXMLIDByCode(CATALOG_IBLOCK_ID, "STATE", "net_v_nal")));
+                    $view_link = sprintf("https://www.alpinabook.ru/bitrix/admin/iblock_element_edit.php?IBLOCK_ID=%d&type=catalog&ID=%d", CATALOG_IBLOCK_ID, $ID);
+                    $ar_template = array(
+                        "NAME" => $ar_product['NAME'],
+                        "URL"  => $view_link
+                    );
+                    CEvent::Send("CATALOG_PRODUCT_NOT_AVAILABLE", array("ru"), $ar_template);
+                }
+            }
+        }
+    }
+
+    function QuantityOnMoreThanZero($ID, $arFields) {
+        if($arFields['QUANTITY'] > 0 && CModule::IncludeModule('iblock')) {
+            $arSelect = Array("CATALOG_QUANTITY", "PROPERTY_STATE", "NAME");
+            $arFilter = Array("ID" => $ID, "IBLOCK_ID" => CATALOG_IBLOCK_ID, "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y");
+            $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
+            if ($ob = $res->GetNextElement()) {
+                $ar_product = $ob->GetFields();
+                //РџСЂРѕРІРµСЂРёРј РЅРµ СЏРІР»СЏРµС‚СЃСЏ Р»Рё РїСЂРµРґР·Р°РєР°Р·РѕРј Рё РЅРµ Р±С‹Р» Р»Рё СЂР°РЅРµРµ РѕС‚СЃС‚Р°С‚РѕРє РјРµРЅСЊС€Рµ 0, РїРѕСЃР»Рµ С‡РµРіРѕ РѕС‚РїСЂР°РІРёРј СЃРѕРѕР±С‰РµРЅРёРµ Рё РїРѕРјРµРЅСЏРµРј СЃС‚Р°С‚СѓСЃ
+                if(($ar_product['PROPERTY_STATE_ENUM_ID'] == getXMLIDByCode(CATALOG_IBLOCK_ID, "STATE", "net_v_nal"))) {
+                    //РЈСЃС‚Р°РЅРѕРІРёРј РЅРѕРІРѕРµ Р·РЅР°С‡РµРЅРёРµ РґР»СЏ РґР°РЅРЅРѕРіРѕ СЃРІРѕР№СЃС‚РІР° РґР°РЅРЅРѕРіРѕ СЌР»РµРјРµРЅС‚Р°
+                    CIBlockElement::SetPropertyValuesEx($ID, false, array('STATE' => getXMLIDByCode(CATALOG_IBLOCK_ID, "STATE", "")));
+                    $view_link = sprintf("https://www.alpinabook.ru/bitrix/admin/iblock_element_edit.php?IBLOCK_ID=%d&type=catalog&ID=%d", CATALOG_IBLOCK_ID, $ID);
+                    $ar_template = array(
+                        "NAME" => $ar_product['NAME'],
+                        "URL"  => $view_link
+                    );
+                    CEvent::Send("CATALOG_PRODUCT_AVAILABLE", array("ru"), $ar_template);
+                }
+            }
+        }
+    }
+}
+
+class Exchange1C {
+    function SyncProductQuantity($ID, $arFields) {
+        logger('Catalog:', $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+        logger($ID, $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+        logger($arFields, $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+
+        //РџРµСЂРІС‹Р№ Р·Р°РїСЂРѕСЃ РґР»СЏ РїРѕР»СѓС‡РµРЅРёСЏ Р·РЅР°С‡РµРЅРёСЏ Bitrix ID
+        $arSelect = Array("PROPERTY_ID_BITRIKS");
+        $arFilter = Array("ID"=>$ID, "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y");
+        $res = CIBlockElement::GetList(Array(), $arFilter, false, Array(), $arSelect);
+        while($arResult = $res->Fetch()) {
+            $bitrix_id = $arResult['PROPERTY_ID_BITRIKS_VALUE'];
+        }
+
+        logger('$bitrix_id:', $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+        logger($bitrix_id, $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+
+        if(!empty($bitrix_id)) {
+
+            logger('$bitrix_id:', $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+            logger($bitrix_id, $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+
+            //Р—Р°РїСЂРѕСЃ РґР»СЏ РїРѕР»СѓС‡РµРЅРёСЏ РѕСЃС‚Р°С‚РєРѕРІ Сѓ СЌР»РµРјРµРЅС‚РѕРІ, РєРѕС‚РѕСЂС‹Рµ РїСЂРёРІСЏР·Р°РЅС‹ Рє С‚РѕРјСѓ Р¶Рµ С‚РѕРІР°СЂСѓ РІ РєР°С‚Р°Р»РѕРіРµ
+            $arSelect = Array("CATALOG_QUANTITY");
+            $arFilter = Array("IBLOCK_ID"=>IBLOCK_1C_EXCHANGE, "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y", "PROPERTY_ID_BITRIKS_VALUE" => $bitrix_id);
+            $cat = CIBlockElement::GetList(Array(), $arFilter, false, Array(), $arSelect);
+            while($arCatalog = $cat->Fetch()) {
+                $total_quantity = $total_quantity + $arCatalog['CATALOG_QUANTITY'];
+            }
+
+            logger('$total_quantity:', $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+            logger($total_quantity, $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+
+            //Р—Р°РїСЂРѕСЃРµ РЅР° РѕР±РЅРѕРІР»РµРЅРёРµ РѕСЃС‚Р°С‚РєРѕРІ Сѓ С‚РѕРІР°СЂР°
+            $arFields = array('QUANTITY' => $total_quantity);// Р·Р°СЂРµР·РµСЂРІРёСЂРѕРІР°РЅРЅРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ
+            $bitrix_id_elem_info = CIBlockElement::GetList (array(), array("IBLOCK_ID" => CATALOG_IBLOCK_ID, "ID" => $bitrix_id), false, false, array("IBLOCK_ID", "ID", "PROPERTY_STATE"));
+            while ($bitrix_id_elem = $bitrix_id_elem_info -> Fetch()) {
+                $state_prop_enum_id = $bitrix_id_elem["PROPERTY_STATE_ENUM_ID"];
+            }
+            if ($state_prop_enum_id != getXMLIDByCode (CATALOG_IBLOCK_ID, "STATE", "soon")) {
+                CCatalogProduct::Update($bitrix_id, $arFields);
+            }
+        }
+    }
+
+    function SyncProductQuantityIblock($arFields) {
+
+        logger('Iblock:', $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+        logger($arFields, $_SERVER["DOCUMENT_ROOT"].'/logs/log_1c.log');
+
+    }
+}*/
