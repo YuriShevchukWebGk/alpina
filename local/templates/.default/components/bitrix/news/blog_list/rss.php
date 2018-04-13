f930e84cc8a7428c083b486d5bc646073fa2d83b
Admin <git@webgk.ru>
2018-04-13 11:02:17 +0300

    SUP-7688: edit page rss

--- local/templates/.default/components/bitrix/news/blog_list/rss.php
+++ local/templates/.default/components/bitrix/news/blog_list/rss.php
@@ -14,7 +14,7 @@ $this->setFrameMode(false);
 ?>
 <?$APPLICATION->IncludeComponent(
 	"bitrix:rss.out",
-	"",
+	"default",
 	Array(
 		"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
 		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
