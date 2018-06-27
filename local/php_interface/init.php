33ca6c9f90e119fdeae4319c5171cc0eb5324907
Sergey <st@webgk.ru>
2018-06-27 16:35:08 +0300

    SUP-7989: addind status update

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -4283,12 +4283,12 @@ AddEventHandler("iblock", "OnAfterIBlockElementDelete", "DeleteElementWishList")
             }    
         }
     }
- function expected_payment(){
+ function expected_payment(){       // автоматическая смена статуса у заказов с онлайн оплатой
     if (CModule::IncludeModule("sale")){
-        $date = date('d.m.Y H:i:s');
-        $time = strtotime("now -1 hour");
-        $date = date('d.m.Y H:i:s',$time);
-        $date_day = date('d.m.Y H:i:s',strtotime("now -1 day"));
+        $date = date('d.m.Y H:i:s');        // текущая дата
+        $time = strtotime("now -1 hour");  // если прошел час
+        $date = date('d.m.Y H:i:s',$time); // присваиваем к текущей дате
+        $date_day = date('d.m.Y H:i:s',strtotime("now -3 hour")); // дата начала поиска
        $arFilter = Array(
           "<=DATE_INSERT" => $date,
           ">=DATE_INSERT" => $date_day,
@@ -4299,9 +4299,9 @@ AddEventHandler("iblock", "OnAfterIBlockElementDelete", "DeleteElementWishList")
        
        while ($arSales = $rsSales->Fetch()) {
            if(($arSales["PAY_SYSTEM_ID"] == RFI_PAYSYSTEM_ID || $arSales["PAY_SYSTEM_ID"] == SBERBANK_PAYSYSTEM_ID) && $arSales["PERSON_TYPE_ID"] == NATURAL_ENTITY_PERSON_TYPE_ID){    
-                   CSaleOrder::StatusOrder($arSales["ID"], "O");
+                   CSaleOrder::StatusOrder($arSales["ID"], "O");   // изменяем статус на "ожидается оплата"
            }
        }  
     }
-    return 'expected_payment();';
+    return 'expected_payment();';    // возвращаем функцию для агента
  }
