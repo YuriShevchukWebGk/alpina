c4780cf933fd8c7f92cd2da03b948468f9e41bfd
Admin <git@webgk.ru>
2018-07-04 12:05:46 +0300

    SUP-7997: add text and style for pages

---
+++ factory3/index.php
@@ -0,0 +1,25 @@
+<style>
+@font-face{font-family:Walshein_light;src:url(/fonts/GT-Walsheim-Pro-Light.eot);src:url(/fonts/GT-Walsheim-Pro-Light.woff) format('woff'),url(/fonts/GT-Walsheim-Pro-Light.ttf) format('truetype');font-weight:400;font-style:normal}
+
+body {
+    font-family: Walshein_light;
+}
+h1 {
+    font-size: 14px;
+}
+a {
+    font-size: 14px;
+    text-decoration: none;
+}
+
+div {
+    width: 560px;
+}
+</style>
+<div>
+<h1>Видео к книге: "Руководство по планированию промышленного предприятия: Стратегия, концепция и осуществление преобразований предприятий, способных к преобразованиям"</h1>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Dvp4nWB3ufI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<br>
+<br>
+<a href="https://www.alpinabook.ru/catalog/GeneralManagment/457120/">Подробнее о книге "Руководство по планированию промышленного предприятия: Стратегия, концепция и осуществление преобразований предприятий, способных к преобразованиям"</a>
+</div>
